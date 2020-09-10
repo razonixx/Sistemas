0fc28b6fc4c4c700a87ef37ed3d7ed89202ef140
root <root@RTX-2080.localdomain>
2020-09-09 21:33:21 -0500

    Data Analysis

--- data_analysis.py
+++ data_analysis.py
@@ -4,17 +4,114 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.mlab import psd
+
+def timeGraph(_start_samp, _end_samp, _channel, _time, pltTitle, pltInstance):
+    pltInstance.plot(_time[_start_samp:_end_samp], _channel[_start_samp:_end_samp])
+    pltInstance.set_xlabel('Tiempo (s)')
+    pltInstance.set_ylabel('micro V')
+    pltInstance.set_title(pltTitle)
+
+def psdGraph(_channel, _win_size, _start_samp, _samp_rate, pltTitle, pltInstance):
+    end_samp = _start_samp + _win_size
+
+    x = _channel[_start_samp : end_samp]
+
+    power, freq = psd(x, NFFT = _win_size, Fs = _samp_rate)   
+
+    start_index = np.where(freq >= 4.0)[0][0]
+    end_index = np.where(freq >= 60.0)[0][0]
+
+    pltInstance.plot(freq[start_index:end_index], power[start_index:end_index])
+    pltInstance.set_xlabel('Hz')
+    pltInstance.set_ylabel('Power')
+    pltInstance.set_title(pltTitle)
+
+def fourGraphs(_training_samples, _channel1, _channel2, _time, _samp_rate, _psd_win_size, _mark, _window):
+    fig, axes = plt.subplots(nrows=2, ncols=2, sharex=False, sharey=False)
+
+    timeGraph(_training_samples[_mark][_window][0], _training_samples[_mark][_window][1], _channel1, _time, "Mark: " + str(_mark) + ", Channel: 1, Window: " + str(_window+1), axes[0, 0])
+    timeGraph(_training_samples[_mark][_window][0], _training_samples[_mark][_window][1], _channel2, _time, "Mark: " + str(_mark) + ", Channel: 2, Window: " + str(_window+1), axes[0, 1])
+    psdGraph(_channel1, _psd_win_size, _training_samples[_mark][_window][0], _samp_rate, "PSD", axes[1, 0])
+    psdGraph(_channel2, _psd_win_size, _training_samples[_mark][_window][0], _samp_rate, "PSD", axes[1, 1])
+
+    fig.tight_layout()
+    plt.show()
+
+def psdCalc(_channel, _win_size, _start_samp, _samp_rate, accumPSDPower, accumPSDFreq):
+    end_samp = _start_samp + _win_size
+
+    x = _channel[_start_samp : end_samp]
+
+    power, freq = psd(x, NFFT = _win_size, Fs = _samp_rate)   
+
+    start_index = np.where(freq >= 4.0)[0][0]
+    end_index = np.where(freq >= 60.0)[0][0]
+
+    accumPSDPower.append(power[start_index:end_index])
+    accumPSDFreq.append(freq[start_index:end_index])
+
+def psdCalcAccum(_training_samples, _channel1, _channel2, _samp_rate, accumPSDFreq, finalPSDPower):
+    accumPSDPowerChan1 = {}
+    accumPSDPowerChan2 = {}
+
+    for mark in _training_samples:
+        if not mark in accumPSDPowerChan1.keys():
+                accumPSDPowerChan1[mark] = []
+                accumPSDPowerChan2[mark] = []
+        for window in _training_samples[mark]:
+            psdCalc(_channel1, 256, window[0], samp_rate, accumPSDPowerChan1[mark], accumPSDFreq)
+            psdCalc(_channel2, 256, window[0], samp_rate, accumPSDPowerChan2[mark], accumPSDFreq)
+
+    print(len(accumPSDPowerChan1[101]))
+    exit()
+
+    '''for i in range(0, len(accumPSDPowerChan1)):
+        for j in range(0, len(accumPSDPowerChan1[i])):
+            finalPSDPowerChan1[j] = finalPSDPowerChan1[j] + accumPSDPowerChan1[i][j]
+            finalPSDPowerChan2[j] = finalPSDPowerChan2[j] + accumPSDPowerChan2[i][j]
+
+    for i in range(0, len(accumPSDPowerChan1)):
+        finalPSDPowerChan1[i] = finalPSDPowerChan1[i]/len(accumPSDPowerChan1[0])
+        finalPSDPowerChan2[i] = finalPSDPowerChan2[i]/len(accumPSDPowerChan2[0])'''
+    
+# Graficar 3 psd, 1 por cada marca
+def fourGraphsAccum(_training_samples, _channel1, _channel2, _time, _samp_rate):
+    fig, axes = plt.subplots(nrows=2, ncols=2, sharex=False, sharey=False)
+    accumPSDFreq = []
+    finalPSDPower = {}
+    #finalPSDPowerChan2 = [0] * 56
+
+    psdCalcAccum(training_samples, chann1, chann2, samp_rate, accumPSDFreq, finalPSDPower)
+
+    axes[0,0].plot(_time, _channel1)
+    axes[0,0].set_xlabel('Tiempo (s)')
+    axes[0,0].set_ylabel('micro V')
+    axes[0,0].set_title('Channel 1')
+
+    axes[0,1].plot(_time, _channel2)
+    axes[0,1].set_xlabel('Tiempo (s)')
+    axes[0,1].set_ylabel('micro V')
+    axes[0,1].set_title('Channel 2')
+
+    axes[1,0].plot(accumPSDFreq[0], finalPSDPowerChan1)
+    axes[1,0].set_xlabel('Hz')
+    axes[1,0].set_ylabel('Power')
+    axes[1,0].set_title("Average PSD")
+
+    axes[1,1].plot(accumPSDFreq[0], finalPSDPowerChan2)
+    axes[1,1].set_xlabel('Hz')
+    axes[1,1].set_ylabel('Power')
+    axes[1,1].set_title("Average PSD")
+
+    fig.tight_layout()
+    plt.show()
 
 # Read data file
 data = np.loadtxt("Izquierda, derecha, cerrado.txt") 
 samp_rate = 256
 samps = data.shape[0]
 n_channels = data.shape[1]
-
-print('Número de muestras: ', data.shape[0])
-print('Número de canales: ', data.shape[1])
-print('Duración del registro: ', samps / samp_rate, 'segundos')
-print(data);
 
 # Time channel
 time = data[:, 0]
@@ -29,8 +126,6 @@ mark = data[:, 6]
 training_samples = {}
 for i in range(0, samps):       
     if mark[i] > 0: 
-        print("Marca", mark[i], 'Muestra', i, 'Tiempo', time[i]) 
-
         if  (mark[i] > 100) and (mark[i] < 200):
             iniSamp = i
             condition_id = mark[i]
@@ -39,42 +134,10 @@ for i in range(0, samps):
                 training_samples[condition_id] = []
             training_samples[int(condition_id)].append([iniSamp, i])
 
-print('Rango de muestras con datos de entrenamiento:', training_samples)
+fourGraphs(training_samples, chann1, chann2, time, 256, 256, 101, 4)
+#fourGraphs(training_samples, chann1, chann2, time, 256, 256, 102, 2)
+#fourGraphs(training_samples, chann1, chann2, time, 256, 256, 103, 5)
 
-# Plot data
-start_samp = training_samples[103][2][0];
-end_samp = training_samples[103][2][1];
+fourGraphsAccum(training_samples, chann1, chann2, time, samp_rate)
 
-plt.plot(time[start_samp:end_samp], chann1[start_samp:end_samp], label = 'Canal 1')
-plt.plot(time[start_samp:end_samp], chann2[start_samp:end_samp], color = 'red', label = 'Canal 2')
-plt.xlabel('Tiempo (s)')
-plt.ylabel('micro V')
-plt.legend()
-plt.show()
 
-# Power Spectral Density (PSD) (1 second of training data)
-win_size = 256
-ini_samp = training_samples[103][2][0]
-end_samp = ini_samp + win_size
-x = chann1[ini_samp : end_samp]
-t = time[ini_samp : end_samp]
-
-plt.plot(t, x)
-plt.xlabel('Tiempo (s)')
-plt.ylabel('micro V')
-plt.show()
-
-power, freq = plt.psd(x, NFFT = win_size, Fs = samp_rate)
-plt.clf()
-
-start_freq = next(x for x, val in enumerate(freq) if val >= 4.0);
-end_freq = next(x for x, val in enumerate(freq) if val >= 60.0);
-print(start_freq, end_freq)
-
-start_index = np.where(freq >= 4.0)[0][0]
-end_index = np.where(freq >= 60.0)[0][0]
-
-plt.plot(freq[start_index:end_index], power[start_index:end_index])
-plt.xlabel('Hz')
-plt.ylabel('Power')
-plt.show()
